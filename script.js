1bd9866980ecf4e22e87edbb61c254956ebd78db
Evgeny <48516892+Parmon101@users.noreply.github.com>
2021-02-24 19:52:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // ----- слайдер верх ----- 
 let slides = document.querySelectorAll('#slides .slide');
 let currentSlide = 0;
-// let slideInterval = setInterval(nextSlide,2000);
 let button = document.querySelector('button')
 
-button.addEventListener('click', function nextSlide(){
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-  })
+function nextSlide(){
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide+1)%slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
+
+button.addEventListener('click', nextSlide)
 
 
 
